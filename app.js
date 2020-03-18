caecfa7fd1890ca2e20f7c971ffb5ef3dc522737
Sebastian Njose <sebastian_njose@hotmail.com>
2020-03-18 21:38:41 +0300

    Setup Pug to be the template engine.

---
+++ app.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => res.render('home', { name: 'Gustavo' }))
+
+app.listen(3000, () => console.log('Server ready'))
